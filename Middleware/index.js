f738cfd7684387d9c635cfcce8095ff4b7d6ed73
faishal siddiqui <faishalsiddiqui759@gmail.com>
2024-12-24 22:50:53 +0530

    middleware completed

---
+++ Middleware/index.js
@@ -0,0 +1,24 @@
+const express = require("express"); 
+const app = express();
+
+let port = 8080;
+
+app.listen(port, ()=>{
+     console.log(`app is listening on port ${port}`); 
+    });
+
+app.use((req, res, next)=>{ 
+    console.log("1st middleware"); next(); 
+});
+
+app.use((req, res, next)=>{ 
+    console.log("2nd middleware"); next();
+});
+
+app.get("/", (req, res)=>{ 
+    res.send("1 response"); 
+});
+
+app.get("/get", (req, res)=>{ 
+    res.send("2nd response"); 
+});
