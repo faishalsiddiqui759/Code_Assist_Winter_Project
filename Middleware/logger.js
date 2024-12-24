64fef5d6796a5f00981f7c0d261d9d7c0fb29df7
faishal siddiqui <faishalsiddiqui759@gmail.com>
2024-12-24 23:01:34 +0530

    logger

---
+++ Middleware/logger.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, ()=>{
+    console.log("app.listening on port : 8080");
+});
+
+app.use((req, res, next)=>{
+    req.responseTime = new Date(Date.now()).toString();
+    console.log(req.method, req.responseTime, req.hostname);
+    next();
+});
+
+app.get("/",(req,res)=>{
+    res.send("response sended");
+});
+
